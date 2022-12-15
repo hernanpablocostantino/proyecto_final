fe29472c0f77b43ba39bfe6a48caefdc81d356fb
hernanpablocostantino <admhernancostantino@gmail.com>
2022-12-15 18:16:55 -0300

    modificación del primer commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,31 @@
 import './Home.css';
 import ListItemHome from "../ListItemHome/ListItemHome";
+//import {useState} from 'react'
 
 
 export default function Home () {
   return (
-    <header className='menu'>
-      <h1>Proyecto - Rick & Morty</h1>
+    // let informacion={
+    //   email: "sjahjdf@hsdfgddy.com",
+    //   telefono: 6546
+    // }
+    // let [unValor,setValor]=useState(true);
+    // let unValor=[true];
+    // const cambiarValor=()=>{
+    //   console.log("se cambio el valor de la variable unValor")
+    //   setValor(false)
+    // }
+    // < className="d-flex justify-content-center align-items-center min-vh-100">
+    <div className="container d-flex justify-content-center align-items-center min-vh-100">
+    <header className="container-home d-flex flex-column justify-content-center align-items-center">
+      <h1 className="title-home text-center">Proyecto - Rick & Morty</h1>
       <nav>
-        <ul className='menu_items'>
-          <ListItemHome dato="Home"/>
+        <ul className="home-items">
           <ListItemHome dato="Characters"/>
           <ListItemHome dato="Contact"/>
         </ul>
       </nav>
     </header>
+    </div> 
   )
 }
